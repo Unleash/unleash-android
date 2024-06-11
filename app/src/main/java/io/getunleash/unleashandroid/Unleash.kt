f06b82ee6ab947d45651b07bd7e51bf80894a64a
Simon Hornby <liquidwicked64@gmail.com>
2024-06-11 15:49:51 +0200

    chore: reasonable definition for context objects

--- app/src/main/java/io/getunleash/unleashandroid/Unleash.kt
+++ app/src/main/java/io/getunleash/unleashandroid/Unleash.kt
@@ -1,5 +1,6 @@
 package io.getunleash.unleashandroid
 
+import io.getunleash.unleashandroid.data.Context
 import io.getunleash.unleashandroid.data.Variant
 
 interface Unleash {
@@ -7,4 +8,6 @@ interface Unleash {
     fun isEnabled(toggleName: String, defaultValue: Boolean = false): Boolean
 
     fun getVariant(toggleName: String, defaultValue: Variant);
+
+    fun setContext(context: Context);
 }
---
+++ app/src/main/java/io/getunleash/unleashandroid/data/UnleashContext.kt
@@ -0,0 +1,71 @@
+package io.getunleash.unleashandroid.data
+
+/**
+ *
+ * Allows the consumer to set the properties that will determine whether a flag is on or off for complex strategies and constraints
+ * Note that this SDK does not support the current time since this will not work correctly with a proxy SDK. The environment property is no
+ * longer exposed, as of v1.0 of this SDK, it's not calculated through the API key.
+ * [See the documentation](https://docs.getunleash.io/docs/user_guide/unleash_context)
+ * @property userId Whatever implementation of userId you use, for stickiness and/or gradual rollouts to work correctly you'll need to set either this or sessionId (can be both)
+ * @property sessionId Whatever implementation of sessionId you use, for stickiness and/or gradual rollouts to work correctly you'll need to set either this or userId  (can be both)
+ * @property remoteAddress the Ip address of the client. If your feature uses the remoteAddress strategy
+ * you'll need to set this
+ * @property properties - Other properties for constraints.
+ * @property appName - The name of your app, used for evaluating strategies - defaults to the one set in the [io.getunleash.UnleashConfig]
+ */
+data class UnleashContext(
+    val userId: String? = null,
+    val sessionId: String? = null,
+    val remoteAddress: String? = null,
+    val appName: String? = null,
+    val properties: Map<String, String> = emptyMap(),
+) {
+    /**
+     * Used to get a new builder with current state
+     */
+    fun newBuilder(): Builder = Builder(
+        userId = userId,
+        sessionId = sessionId,
+        remoteAddress = remoteAddress,
+        properties = properties.toMutableMap(),
+        appName = appName,
+    )
+
+    companion object {
+        /**
+         * Used to get a Builder with no fields set, hopefully simplifying constructing contexts
+         */
+        fun newBuilder(): Builder = Builder()
+    }
+
+    data class Builder(
+        var userId: String? = null,
+        var sessionId: String? = null,
+        var remoteAddress: String? = null,
+        var properties: MutableMap<String, String> = mutableMapOf(),
+        var appName: String? = null,
+    ) {
+
+        fun userId(userId: String) = apply { this.userId = userId }
+
+        fun sessionId(sessionId: String) = apply { this.sessionId = sessionId }
+
+        fun remoteAddress(address: String) = apply { this.remoteAddress = address }
+
+        fun addProperty(key: String, value: String) = apply { this.properties[key] = value }
+
+        fun properties(map: MutableMap<String, String>) = apply { this.properties = map }
+
+        fun appName(appName: String) = apply { this.appName = appName }
+
+        fun build(): UnleashContext {
+            return UnleashContext(
+                userId = userId,
+                sessionId = sessionId,
+                remoteAddress = remoteAddress,
+                properties = properties.toMap(),
+                appName = appName,
+            )
+        }
+    }
+}
