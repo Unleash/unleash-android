37d7eabce814c46fc323ec2c572514061afcf1c8
Gastón Fournier <gaston@getunleash.io>
2024-06-13 10:36:18 +0200

    Work manager available

--- unleashandroidsdk/build.gradle.kts
+++ unleashandroidsdk/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.work.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
