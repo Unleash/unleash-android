705ba4525efc5ad867f5ae5426bef53bea1db883
Gastón Fournier <gaston@getunleash.io>
2024-07-11 22:33:52 +0200

    Add timestamp to log output in tests

--- unleashandroidsdk/src/test/java/io/getunleash/android/BaseTest.kt
+++ unleashandroidsdk/src/test/java/io/getunleash/android/BaseTest.kt
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowLog
+import java.text.SimpleDateFormat
+import java.util.Date
 
 @RunWith(RobolectricTestRunner::class)
 @Config(manifest= Config.NONE, sdk = [21])
@@ -13,6 +15,12 @@ abstract class BaseTest {
     @Before
     fun setup() {
         ShadowLog.stream = System.out
+        // Add a timestamp to the log output
+        val df  = SimpleDateFormat("[yyyy-MM-dd'T'HH:mm:ss.SSSZ]")
+        ShadowLog.setTimeSupplier {
+            df.format(Date())
+        }
+
     }
 }
 
