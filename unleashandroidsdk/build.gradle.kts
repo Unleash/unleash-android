af4d9e2c97710054be5ec2d1b1a3b05f8c4d8f2a
Gastón Fournier <gaston@getunleash.io>
2024-07-18 11:44:44 +0200

    chore: publish artifact to maven configuration (#28)

--- unleashandroidsdk/build.gradle.kts
+++ unleashandroidsdk/build.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
+    `maven-publish`
+    signing
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("org.jetbrains.dokka") version "1.7.10"
+    id("pl.allegro.tech.build.axion-release") version "1.13.6"
 }
+
+val tagVersion = System.getenv("GITHUB_REF")?.split('/')?.last()
+project.version = scmVersion.version
 
 android {
     namespace = "io.getunleash.android"
@@ -10,11 +17,17 @@ android {
     defaultConfig {
         minSdk = 21
 
+        aarMetadata {
+            minCompileSdk = 29
+        }
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+        consumerProguardFiles("proguard-rules.pro")
     }
 
     buildTypes {
+        debug {
+
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -30,6 +43,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    publishing {
+        multipleVariants {
+            includeBuildTypeValues("debug", "release")
+            allVariants()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -37,11 +58,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.work.ktx)
+    implementation(libs.androidx.lifecycle.process)
     implementation(libs.jackson.databind)
     implementation(libs.jackson.core)
     implementation(libs.jackson.module.kotlin)
     implementation(libs.jackson.datatype.jsr310)
-    implementation(libs.androidx.lifecycle.process)
     api(libs.okhttp)
 
     testImplementation(libs.junit)
@@ -58,4 +79,69 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.okhttp.mockserver)
+}
+
+publishing {
+    repositories {
+        repositories {
+            maven {
+                url = uri(layout.buildDirectory.dir("repo"))
+                name = "test"
+            }
+        }
+        mavenLocal()
+    }
+
+    publications {
+        afterEvaluate {
+            create<MavenPublication>("mavenJava") {
+                from(components["release"])
+                groupId = "io.getunleash"
+                artifactId = "unleash-android"
+                version = version
+                pom {
+                    name.set("Unleash Android")
+                    description.set("Android SDK for Unleash")
+                    url.set("https://gh.getunleash.io/unleash-android")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("gastonfournier")
+                            name.set("Gastón Fournier")
+                            email.set("gaston@getunleash.io")
+                        }
+                        developer {
+                            id.set("chrkolst")
+                            name.set("Christopher Kolstad")
+                            email.set("chriswk@getunleash.io")
+                        }
+                        developer {
+                            id.set("ivarconr")
+                            name.set("Ivar Conradi Østhus")
+                            email.set("ivarconr@getunleash.io")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/Unleash/unleash-android")
+                        developerConnection.set("scm:git:ssh://git@github.com:Unleash/unleash-android")
+                        url.set("https://github.com/Unleash/unleash-android")
+                    }
+                }
+            }
+        }
+    }
+}
+
+val signingKey: String? by project
+val signingPassphrase: String? by project
+signing {
+    if (signingKey != null && signingPassphrase != null) {
+        useInMemoryPgpKeys(signingKey, signingPassphrase)
+        sign(publishing.publications["mavenJava"])
+    }
 }
