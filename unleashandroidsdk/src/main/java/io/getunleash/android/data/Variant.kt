27b585f91915950f02ecf1ba388887a0a88c1f87
Gastón Fournier <gaston@getunleash.io>
2024-10-15 11:14:29 +0200

    fix: variant enabled should be false by default (#93)
    This complies with other SDKs. For reference we use NodeJS:
    ```typescript
    const defaultVariant: IVariant = {
        name: 'disabled',
        enabled: false,
        feature_enabled: false,
    };
    ```

--- unleashandroidsdk/src/main/java/io/getunleash/android/data/Variant.kt
+++ unleashandroidsdk/src/main/java/io/getunleash/android/data/Variant.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 
 data class Variant(
     val name: String,
-    val enabled: Boolean = true,
+    val enabled: Boolean = false,
     @Json(name = "feature_enabled") val featureEnabled: Boolean = false,
     val payload: Payload? = null
 )
