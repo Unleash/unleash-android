0c2549a17e22782ec9f8eccec03831f1853aa5dc
kwasniew <kwasniewski.mateusz@gmail.com>
2025-01-22 13:15:28 +0100

    feat: client identification headers

--- unleashandroidsdk/src/main/java/io/getunleash/android/UnleashConfig.kt
+++ unleashandroidsdk/src/main/java/io/getunleash/android/UnleashConfig.kt
@@ -44,8 +44,11 @@ data class UnleashConfig(
             "Authorization" to auth,
             "Content-Type" to "application/json",
             "UNLEASH-APPNAME" to appName,
+            "X-UNLEASH-APPNAME" to appName,
             "User-Agent" to appName,
             "UNLEASH-INSTANCEID" to instanceId,
+            "X-UNLEASH-CONNECTION-ID" to instanceId,
+            "X-UNLEASH-SDK" to "unleash-client-android:development",
         ))
     }
 
